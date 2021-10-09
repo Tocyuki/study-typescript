1a13b7eec35657af080c9b2413c7b665206378cc
Toshiyuki Kasai <tocguitar1@gmail.com>
2021-10-10 02:49:22 +0900

    create subnets

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core';
-import { CfnVPC } from '@aws-cdk/aws-ec2';
+import { CfnVPC, CfnSubnet } from '@aws-cdk/aws-ec2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +9,42 @@ export class CdkStack extends cdk.Stack {
       cidrBlock: '10.0.0.0/16',
       tags: [{
         key: 'Name', value: 'study-typescript-vpc'
+      }]
+    });
+
+    const publicSubnetA = new CfnSubnet(this, 'PublicSubnetA', {
+      cidrBlock: '10.0.1.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{
+        key: 'Name', value: 'study-typescript-public-subnet-a'
+      }]
+    });
+
+    const publicSubnetC = new CfnSubnet(this, 'PublicSubnetC', {
+      cidrBlock: '10.0.2.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{
+        key: 'Name', value: 'study-typescript-public-subnet-c'
+      }]
+    });
+
+    const privateSubnetA = new CfnSubnet(this, 'PrivateSubnetA', {
+      cidrBlock: '10.0.11.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{
+        key: 'Name', value: 'study-typescript-private-subnet-a'
+      }]
+    });
+
+    const privateSubnetC = new CfnSubnet(this, 'PrivateSubnetC', {
+      cidrBlock: '10.0.12.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{
+        key: 'Name', value: 'study-typescript-private-subnet-c'
       }]
     });
   }
--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -14,3 +14,30 @@ test('Vpc', () => {
         }]
     }));
 });
+
+test('Subnet', () => {
+    const app = new cdk.App();
+    const stack = new Cdk.CdkStack(app, 'MyTestStack');
+
+    expect(stack).to(countResources('AWS::EC2::Subnet', 4));
+    expect(stack).to(haveResource('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.1.0/24',
+        AvailabilityZone: "ap-northeast-1a",
+        Tags: [{ 'Key': 'Name', 'Value': 'study-typescript-public-subnet-a' }]
+    }));
+    expect(stack).to(haveResource('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.2.0/24',
+        AvailabilityZone: "ap-northeast-1c",
+        Tags: [{ 'Key': 'Name', 'Value': 'study-typescript-public-subnet-c' }]
+    }));
+    expect(stack).to(haveResource('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.11.0/24',
+        AvailabilityZone: "ap-northeast-1a",
+        Tags: [{ 'Key': 'Name', 'Value': 'study-typescript-private-subnet-a' }]
+    }));
+    expect(stack).to(haveResource('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.12.0/24',
+        AvailabilityZone: "ap-northeast-1c",
+        Tags: [{ 'Key': 'Name', 'Value': 'study-typescript-private-subnet-c' }]
+    }));
+});
