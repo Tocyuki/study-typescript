532ea2e6fdfacb8383f44e673670adddf6faa1cf
Toshiyuki Kasai <tocguitar1@gmail.com>
2021-10-10 02:38:19 +0900

    create vpc

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import { CfnVPC } from '@aws-cdk/aws-ec2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new CfnVPC(this, 'Vpc', {
+      cidrBlock: '10.0.0.0/16',
+      tags: [{
+        key: 'Name', value: 'study-typescript-vpc'
+      }]
+    });
   }
 }
