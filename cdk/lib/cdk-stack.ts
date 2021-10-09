8574362437f975a93e8efdf2c137c13acb36e923
Toshiyuki Kasai <tocguitar1@gmail.com>
2021-10-10 03:00:46 +0900

    modify cdk-stack.ts

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,45 +7,35 @@ export class CdkStack extends cdk.Stack {
 
     const vpc = new CfnVPC(this, 'Vpc', {
       cidrBlock: '10.0.0.0/16',
-      tags: [{
-        key: 'Name', value: 'study-typescript-vpc'
-      }]
+      tags: [{ key: 'Name', value: 'study-typescript-vpc' }]
     });
 
     const publicSubnetA = new CfnSubnet(this, 'PublicSubnetA', {
       cidrBlock: '10.0.1.0/24',
       vpcId: vpc.ref,
       availabilityZone: 'ap-northeast-1a',
-      tags: [{
-        key: 'Name', value: 'study-typescript-public-subnet-a'
-      }]
+      tags: [{ key: 'Name', value: 'study-typescript-public-subnet-a' }]
     });
 
     const publicSubnetC = new CfnSubnet(this, 'PublicSubnetC', {
       cidrBlock: '10.0.2.0/24',
       vpcId: vpc.ref,
       availabilityZone: 'ap-northeast-1c',
-      tags: [{
-        key: 'Name', value: 'study-typescript-public-subnet-c'
-      }]
+      tags: [{ key: 'Name', value: 'study-typescript-public-subnet-c' }]
     });
 
     const privateSubnetA = new CfnSubnet(this, 'PrivateSubnetA', {
       cidrBlock: '10.0.11.0/24',
       vpcId: vpc.ref,
       availabilityZone: 'ap-northeast-1a',
-      tags: [{
-        key: 'Name', value: 'study-typescript-private-subnet-a'
-      }]
+      tags: [{ key: 'Name', value: 'study-typescript-private-subnet-a' }]
     });
 
     const privateSubnetC = new CfnSubnet(this, 'PrivateSubnetC', {
       cidrBlock: '10.0.12.0/24',
       vpcId: vpc.ref,
       availabilityZone: 'ap-northeast-1c',
-      tags: [{
-        key: 'Name', value: 'study-typescript-private-subnet-c'
-      }]
+      tags: [{ key: 'Name', value: 'study-typescript-private-subnet-c' }]
     });
   }
 }
